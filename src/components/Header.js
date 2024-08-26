f68ab2fcb736da6c23634b7362739b9efbfa5dc4
PRATIK12NIPANE <136259872+PRATIK12NIPANE@users.noreply.github.com>
2024-08-26 18:29:17 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
                 <Link to="/" >   <img className=" w-25"  src="/imgs/logo.png" alt="logo here"/> </Link>
             </div>
             <div className="col-sm-5">
-                <b>   <i> Creating Happy Home Owners Everyday </i> </b>
+                <b>   <i> Destination For Happy Home Owners </i> </b>
             </div>
             {/* <div className="col-sm-3">
             <Link to="/EnquiriesList"> <button className="btn btn-primary">Enquiry</button> </Link>
