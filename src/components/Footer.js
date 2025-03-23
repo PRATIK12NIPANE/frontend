71abde833439800b0a01f1220c0a19fb13f228ef
PRATIK12NIPANE <136259872+PRATIK12NIPANE@users.noreply.github.com>
2025-03-23 23:00:03 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (  
         <div className="row bg-warning" >
             <div className="col-sm-12 d-flex justify-content-center">
-                <footer> &copy; <b> 2024 IConnect </b> </footer>
+                <footer> &copy; <b> 2024 Build by @pratiknipane </b> </footer>
             </div>
         </div>
     );
